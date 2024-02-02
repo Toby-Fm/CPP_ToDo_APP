d3560f8c62793d57ed08c854a0437afb7d03b087
Toby-fm <mail@toby-wichmann.de>
2024-02-02 22:07:05 +0100

    Add saveTasksToFile and loadTasksFromFile functions
    This commit adds two new functions, saveTasksToFile and loadTasksFromFile, to the Task.cpp file. These functions allow tasks to be saved to and loaded from a file.

--- CPP_ToDo/Task.cpp
+++ CPP_ToDo/Task.cpp
@@ -1,1 +1,41 @@
 #include "Task.h"
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& filename)
+{
+	std::ofstream ostream(filename);
+	ostream << tasks.size();
+
+	for (const Task& task : tasks)
+	{
+		std::string description = task.description;
+		std::replace(description.begin(), description.end(), ' ', '_');
+
+		ostream << '\n' << description << ' ' << task.done;
+	}
+}
+std::vector<Task> loadTasksFromFile(const std::string& filename)
+{
+	if (!std::filesystem::exists(filename))
+	{
+		return std::vector<Task>();
+	}
+	std::vector<Task> tasks;
+	std::ifstream istream(filename);
+	int n;
+	istream >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::string description;
+		bool done;
+
+		istream >> description >> done;
+		std::replace(description.begin(), description.end(), '_', ' ');
+        tasks.push_back(Task{ description, done });
+	}
+	return tasks;
+}
