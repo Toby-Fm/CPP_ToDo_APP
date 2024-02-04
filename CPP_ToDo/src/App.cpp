e867a7092c3df97af8a7c33caffcb48217c16604
Toby-fm <mail@toby-wichmann.de>
2024-02-04 22:38:31 +0100

    Hinzufügen von Fenster-Symbol

--- CPP_ToDo/src/App.cpp
+++ CPP_ToDo/src/App.cpp
@@ -9,6 +9,12 @@ bool App::OnInit()
 	mainFrame->SetClientSize(800, 600);
 	mainFrame->SetMinSize(wxSize(300, 600));
 	mainFrame->Center();
+	/*
+	// Icon for the window png
+	wxIcon icon;
+	icon.LoadFile(wxT("../images/logo3.png", wxBITMAP_TYPE_PNG));
+	mainFrame->SetIcon(icon);
+	*/
 	mainFrame->Show();
 	return true;
 }
