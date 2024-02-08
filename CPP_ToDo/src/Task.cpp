20fee61f9d27ddd756f5df4899960f17afeef317
Toby-fm <mail@toby-wichmann.de>
2024-02-09 00:47:05 +0100

    Hinzufügen von Funktionen zum Speichern und Laden von erledigten Aufgaben

--- CPP_ToDo/src/Task.cpp
+++ CPP_ToDo/src/Task.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 
-
+// Save and Load Tasks
 void SaveTasksToFile(const std::vector<Task>& tasks, const std::string& filename)
 {
 	std::ofstream ostream(filename);
@@ -38,6 +38,44 @@ std::vector<Task> LoadTasksFromFile(const std::string& filename)
 		istream >> description >> done;
 		std::replace(description.begin(), description.end(), '_', ' ');
         tasks.push_back(Task{ description, done });
+	}
+	return tasks;
+}
+
+
+// Save and Load Done Tasks
+void SaveDoneTasksToFile(const std::vector<Task>& tasks, const std::string& filename)
+{
+	std::ofstream ostream(filename);
+	ostream << tasks.size();
+
+	for (const Task& task : tasks)
+	{
+		std::string description = task.description;
+		std::replace(description.begin(), description.end(), ' ', '_');
+
+		ostream << '\n' << description << ' ' << task.done;
+	}
+}
+std::vector<Task> LoadDoneTasksFromFile(const std::string& filename)
+{
+	if (!std::filesystem::exists(filename))
+	{
+		return std::vector<Task>();
+	}
+	std::vector<Task> tasks;
+	std::ifstream istream(filename);
+	int n;
+	istream >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::string description;
+		bool done;
+
+		istream >> description >> done;
+		std::replace(description.begin(), description.end(), '_', ' ');
+		tasks.push_back(Task{ description, done });
 	}
 	return tasks;
 }
