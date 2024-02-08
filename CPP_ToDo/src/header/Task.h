d47f7e2e054b4427ac9ed34d356eb0712736ffe6
Toby-fm <mail@toby-wichmann.de>
2024-02-09 00:47:10 +0100

    Hinzufügen von Funktionen zum Speichern und Laden von erledigten Aufgaben

--- CPP_ToDo/src/header/Task.h
+++ CPP_ToDo/src/header/Task.h
@@ -8,5 +8,10 @@ struct Task
 	bool done;
 };
 
+// Save and Load Tasks
 void SaveTasksToFile(const std::vector<Task>& tasks, const std::string& filename);
 std::vector<Task> LoadTasksFromFile(const std::string& filename);
+
+// Save and Load Done Tasks
+void SaveDoneTasksToFile(const std::vector<Task>& tasks, const std::string& filename);
+std::vector<Task> LoadDoneTasksFromFile(const std::string& filename);
